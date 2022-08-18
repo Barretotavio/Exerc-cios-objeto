51e2d9e7f336a0acae3a7173c74e3a55336be088
Barretotavio <obarretomachado@gmail.com>
2022-08-18 09:48:01 -0300

    Exercicios utilizando objeto

---
+++ 01/index.js
@@ -0,0 +1,19 @@
+const endereco = {
+    rua: "Jhoaquim Phoenyx",
+    numero: 111,
+    complemento: "Apartamento 502",
+    bairro: "Boa Aguia",
+    cep: 99.888 - 77,
+    cidade: "São Felix",
+    estado: "Bahia",
+    pais: "Brasil"
+};
+
+const usuario = {
+    noome: "Fausto Silva",
+    email: "ogostosofaustosilva@bandi.com.br",
+    telefone: 71988887777,
+    data_nascimento: 22 / 04 / 1954,
+    endereco
+};
+console.log(endereco, usuario);
